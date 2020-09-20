f9d55feb7eb11e71f352cc6ea4ec7859c1cd0135
Caique Pereira Alves <caiquewin@gmail.com>
2020-09-20 15:07:20 -0300

    Exemplo 7 javaScript MEGA Simples

---
+++ Projeto_3/JS/javascript.js
@@ -0,0 +1,7 @@
+document.getElementById('Text1').innerText = 'Texto Alterado 1';
+
+document.getElementById('Text2').innerText = "Texto Alterado 2";
+
+window.onload = function () {
+    document.getElementById('Text3').innerText = "Texto Alterado 3";
+}
