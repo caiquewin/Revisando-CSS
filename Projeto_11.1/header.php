7fd0632f5d7abe6cb1a13ad48b223ab35cf9d9b1
Caique Pereira Alves <caiquewin@gmail.com>
2020-09-22 18:32:37 -0300

    Pagina da sony simples (somente o footer fico pra finalizar)

---
+++ Projeto_11.1/header.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PlayStation® Official Site - PlayStation Console, Games, Accessories - PlayStation</title>
+    <link rel="shortcut icon" href="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/dd40142d-9060-4b80-99ce-c09c8c5d231d/de2cqti-47c92e47-c107-4490-a5bb-7dc3e9332eea.gif?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvZGQ0MDE0MmQtOTA2MC00YjgwLTk5Y2UtYzA5YzhjNWQyMzFkXC9kZTJjcXRpLTQ3YzkyZTQ3LWMxMDctNDQ5MC1hNWJiLTdkYzNlOTMzMmVlYS5naWYifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.FZCV5Z09TcOA_s_2ctU7dc3Z3XZYf8TSetbgQAVm4wc" type="image/x-icon">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/index.css">
+</head>
+
+<body>
+    <div class="container-fluid>
+        <div class="row">
+            <div class="col text-right fundo-body p-2">
+                <img src="assets/img/sony.png" style="height:15px;" class="img-fluid">
+            </div>
+        </div>
+        <div class="row d-flex align-items-center">
+            <div class="col col-md-1 col-lg-1 col-lx1">
+                <a href="http://"><img src="assets/img/playstation.png" style="width: 50px;"></a>
+            </div>
+            <div class="col col-md-1 col-lg-1 col-lg-1">
+                <a href="#" class="teste">Jogos</a>
+            </div>
+            <div class="col col-lg-1 col-lxg-">
+                <a href="#" class="teste">Hardware</a>
+            </div>
+            <div class="col col-lg-1 col-lg-1">
+                <a href="#" class="teste">Serviços</a>
+            </div>
+            <div class="col col-lg-1 col-lg-1">
+                <a href="#" class="teste">Notícias</a>
+            </div>
+            <div class="col col-lg-1 col-lg-1">
+                <a href="#" class="teste">Loja</a>
+            </div>
+            <div class="col col-md-1 col-lg-1 col-lg-1">
+                <a href="#" class="teste">Ajuda</a>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col">
+                <div class="text-center">
+                    <img src="assets/img/playstation5.png" class="img-fluid mx-auto">
+                </div>
+            </div>
+        </div>
+    </div>
